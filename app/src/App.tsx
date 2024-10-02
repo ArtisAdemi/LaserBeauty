955e8206c9c14cad73dcb2359483895b8034a0b3
ArtisAdemi <artis.ademi@gmail.com>
2024-10-02 17:19:05 +0200

    Footer component

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,10 +1,23 @@
 import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import Offers from './pages/Offers'
 function App() {
 
   return (
     <div className='App'>
       <Navbar />
+      <div>
+        <Router>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/offers' element={<Offers />} />
+          </Routes>
+        </Router>
+      </div>
+      <Footer />
     </div>
   )
 }
