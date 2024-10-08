dc2188e4616c27eb5f497370b0af96dd6a8c5c0d
Florian Berisha <florian.fani@hotmail.com>
2024-10-08 12:21:21 +0200

    ofertat done, pricing and translation

--- app/src/pages/Offers.tsx
+++ app/src/pages/Offers.tsx
@@ -1,76 +1,78 @@
-import React, { useEffect, useState } from "react";
-import Legs from "../components/Legs";
-import Arms from "../components/Arms";
-import Body from "../components/Body";
-import Face from "../components/Face";
+import React from "react";
 import pricing from "../assets/pricing-image.jpg";
 
 const Shërbimet: React.FC = () => {
-  const [selectedTab, setSelectedTab] = useState(getInitialTab());
-
-  function getInitialTab() {
-    const tabData = localStorage.getItem("PRICE_LIST");
-    const savedTab = tabData ? JSON.parse(tabData) : null;
-    return savedTab || "Legs";
-  }
-
-  useEffect(() => {
-    const tabData = JSON.stringify(selectedTab);
-    localStorage.setItem("PRICE_LIST", tabData);
-  }, [selectedTab]);
-
   return (
     <div>
-      <div className="h-[500px] relative">
+      <div className="h-[300px] relative">
         <img src={pricing} alt="Contact" className="w-full opacity-70 h-full object-cover" />
 
         <div className="absolute top-1/2 inset-x-0 text-center transform -translate-y-1/2">
           <h1 className="text-5xl text-[#6a483c]">Ofertat</h1>
         </div>
       </div>
-      <div className="px-[8%] xl:px-[16%] flex flex-col justify-center mt-24">
-        <h2 className="text-5xl text-[#6a483c] text-center">Waxing Price List</h2>
-        <p className="xl:px-[20%] text-[#885D4D] text-center mt-6">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis.</p>
-      </div>
-      <div className="transform flex rounded-md w-[324px] mx-auto justify-center z-10 mt-5">
-        <div className="">
-          <button onClick={() => setSelectedTab("Legs")} className={`font-semibold py-4 px-6 rounded-md hover:text-[#ff7404] hover:duration-500 duration-500 ${selectedTab === "Cocktails" ? "bg-transparent text-[#6a483c]" : "text-[#6a483c]"}`}>
-            Legs
-          </button>
-          <div className="h-[1.5px] w-full overflow-hidden">
-            <div className={`orangeline w-full mx-auto border-b border-[1.5px] border-[#ff7404] transition-transform duration-300 ease-out ${selectedTab === "Cocktails" ? "scale-x-100" : "scale-x-0"}`}></div>
-          </div>
-        </div>
-        <div className="flex flex-col">
-          <button onClick={() => setSelectedTab("Arms")} className={`font-semibold py-4 px-6 rounded-md hover:text-[#ff7404] hover:duration-500 duration-500 ${selectedTab === "Food" ? "bg-transparent text-[#6a483c]" : "text-[#6a483c]"}`}>
-            Arms
-          </button>
-          <div className="h-[1.5px] w-full overflow-hidden">
-            <div className={`orangeline w-full mx-auto border-b border-[1.5px] border-[#ff7404] transition-transform duration-300 ease-out ${selectedTab === "Food" ? "scale-x-100" : "scale-x-0"}`}></div>
-          </div>
-        </div>
-        <div className="">
-          <button onClick={() => setSelectedTab("Body")} className={`font-semibold py-4 px-6 rounded-md hover:text-[#ff7404] hover:duration-500 duration-500 ${selectedTab === "Nuts" ? "bg-transparent text-[#6a483c]" : "text-[#6a483c]"}`}>
-            Body
-          </button>
-          <div className="h-[1.5px] w-full overflow-hidden">
-            <div className={`orangeline w-full mx-auto border-b border-[1.5px] border-[#ff7404] transition-transform duration-300 ease-out ${selectedTab === "Nuts" ? "scale-x-100" : "scale-x-0"}`}></div>
-          </div>
-        </div>
-        <div className="">
-          <button onClick={() => setSelectedTab("Face")} className={`font-semibold py-4 px-6 rounded-md hover:text-[#ff7404] hover:duration-500 duration-500 ${selectedTab === "Nuts" ? "bg-transparent text-[#6a483c]" : "text-[#6a483c]"}`}>
-            Face
-          </button>
-          <div className="h-[1.5px] w-full overflow-hidden">
-            <div className={`orangeline w-full mx-auto border-b border-[1.5px] border-[#ff7404] transition-transform duration-300 ease-out ${selectedTab === "Nuts" ? "scale-x-100" : "scale-x-0"}`}></div>
-          </div>
-        </div>
-      </div>
+
       <div className="px-[8%] xl:px-[16%] mb-10">
-        {selectedTab === "Legs" && <Legs />}
-        {selectedTab === "Arms" && <Arms />}
-        {selectedTab === "Body" && <Body />}
-        {selectedTab === "Face" && <Face />}
+        <div className="xl:flex justify-between gap-x-20">
+          <div className="xl:w-1/2">
+            <div className="py-4 border-2 border-[#6a483c] border-t-0 border-l-0 border-r-0 ">
+              <div className="flex justify-between">
+                <h1 className="text-[#6a483c] font-semibold text-2xl">Depilim me Laser</h1>
+                <p className="text-[#6a483c] font-semibold text-2xl">€80</p>
+              </div>
+              <div>
+                <h1 className="text-[#885D4D] mt-3">Komplet trupi (1 Seancë)</h1>
+              </div>
+            </div>
+            <div className="py-4 border-2 border-[#6a483c] border-t-0 border-l-0 border-r-0 ">
+              <div className="flex justify-between">
+                <h1 className="text-[#6a483c] font-semibold text-2xl">Depilim me Laser</h1>
+                <p className="text-[#6a483c] font-semibold text-2xl">€30</p>
+              </div>
+              <div>
+                <h1 className="text-[#885D4D] mt-3">Fytyrën (1 Seancë)</h1>
+              </div>
+            </div>
+            <div className="py-4 border-2 border-[#6a483c] border-t-0 border-l-0 border-r-0 ">
+              <div className="flex justify-between">
+                <h1 className="text-[#6a483c] font-semibold text-2xl">Depilim me Laser</h1>
+                <p className="text-[#6a483c] font-semibold text-2xl">€40</p>
+              </div>
+              <div>
+                <h1 className="text-[#885D4D] mt-3">Nënsqetulla (1 Seancë)</h1>
+              </div>
+            </div>
+          </div>
+          <div className="xl:w-1/2">
+            <div className="py-4 border-2 border-[#6a483c] border-t-0 border-l-0 border-r-0 ">
+              <div className="flex justify-between">
+                <h1 className="text-[#6a483c] font-semibold text-2xl">Depilim me Laser</h1>
+                <p className="text-[#6a483c] font-semibold text-2xl">€40</p>
+              </div>
+              <div>
+                <h1 className="text-[#885D4D] mt-3">Duart (1 Seancë)</h1>
+              </div>
+            </div>
+            <div className="py-4 border-2 border-[#6a483c] border-t-0 border-l-0 border-r-0 ">
+              <div className="flex justify-between">
+                <h1 className="text-[#6a483c] font-semibold text-2xl">Depilim me Laser</h1>
+                <p className="text-[#6a483c] font-semibold text-2xl">€40</p>
+              </div>
+              <div>
+                <h1 className="text-[#885D4D] mt-3">Genital (1 Seancë)</h1>
+              </div>
+            </div>
+            <div className="py-4 xl:border-2 xl:border-[#6a483c] xl:border-t-0 xl:border-l-0 xl:border-r-0 ">
+              <div className="flex justify-between">
+                <h1 className="text-[#6a483c] font-semibold text-2xl">Depilim me Laser</h1>
+                <p className="text-[#6a483c] font-semibold text-2xl">€50</p>
+              </div>
+              <div>
+                <h1 className="text-[#885D4D] mt-3">Këmbët (1 Seancë)</h1>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
